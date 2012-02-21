17fc7b0f7e2dc400028925f1bff9d3a18e2ad569
Sarah Kuehnle <sarah@sooperduper.com>
2012-02-20 21:35:47 -0800

    Assignment 0 - collect first and last name and print

---
+++ ps0.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+ps1a.py
+Problem Set 0
+
+Created by Sarah on 2012-02-20.
+From: MIT OpenCourseWare
+Course: Introduction to Computer Science and Programming
+MIT Course Number: 6.00
+"""
+
+# Ask for the user's first name
+first_name = raw_input('What is your first name? ')
+
+# Ask for the user's last name
+last_name = raw_input('What is your last name? ')
+
+# Print out the users full name
+print "Hello, " + first_name + " " + last_name + "!"
